cb979587cbdc3728c714fc39650662346203f381
RKamey <90283664+RKamey@users.noreply.github.com>
2022-11-27 17:51:17 -0600

    primer commit

---
+++ ArbolBinario.js
@@ -0,0 +1,13 @@
+class Nodo {
+    constructor(valor) {
+        this.valor = valor;
+        this.izq = null;
+        this.der = null;
+        this.sig = null;
+        this.ant = null;
+    }
+}
+
+
+
+
