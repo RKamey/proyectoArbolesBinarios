fb7710f291d547e55d514d49df586cef60162f3c
RKamey <90283664+RKamey@users.noreply.github.com>
2022-11-27 18:09:04 -0600

    Add files via upload

--- ArbolBinario.js
+++ ArbolBinario.js
@@ -1,13 +1,41 @@
 class Nodo {
     constructor(valor) {
         this.valor = valor;
-        this.izq = null;
-        this.der = null;
-        this.sig = null;
-        this.ant = null;
+        this.hijoIzq = null;
+        this.hijoDer = null;
+        this.siguiente = null;
+        this.anterior = null;
     }
 }
 
+class ArbolBinario {
+    constructor(expresion) {
+        this.expresion = expresion;
+        this.raiz = null;
+        this.primero = null;
+        this.ultimo = null;
+    }
 
+    separarExpresion() {
+        let expresion = this.expresion;
+        let separada = expresion.split('');
 
+        for (let i = 0; i < separada.length; i++) {
+            this.agregarNodo(separada[i]);
+        }
+    }
+
+    agregarNodo(valor) {
+        let nodo = new Nodo(valor);
+
+        if (this.primero == null) {
+            this.primero = nodo;
+            this.ultimo = nodo;
+        } else {
+            this.ultimo.siguiente = nodo;
+            nodo.anterior = this.ultimo;
+            this.ultimo = nodo;
+        }
+    }
+}
 
