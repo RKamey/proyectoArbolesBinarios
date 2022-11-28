d816af1c58720a446c842139723ffeb12011a745
RKamey <90283664+RKamey@users.noreply.github.com>
2022-11-28 01:40:13 -0600

    Add files via upload

--- ArbolBinario.js
+++ ArbolBinario.js
@@ -14,28 +14,158 @@ class ArbolBinario {
         this.raiz = null;
         this.primero = null;
         this.ultimo = null;
+        this.preorderArr = [];
+        this.postorderArr = [];
     }
 
     separarExpresion() {
-        let expresion = this.expresion;
-        let separada = expresion.split('');
+        let expresionSeparada = this.expresion.split('');
 
-        for (let i = 0; i < separada.length; i++) {
-            this.agregarNodo(separada[i]);
+        for (let i = 0; i < expresionSeparada.length; i++) {
+            this.agregarNodo(expresionSeparada[i]);
         }
+        return this.crearArbol();
     }
 
-    agregarNodo(valor) {
-        let nodo = new Nodo(valor);
+    agregarNodo(nodo) {
+        let nodox = new Nodo(nodo);
 
         if (this.primero == null) {
-            this.primero = nodo;
-            this.ultimo = nodo;
+            this.primero = nodox;
+            this.ultimo = nodox;
         } else {
-            this.ultimo.siguiente = nodo;
-            nodo.anterior = this.ultimo;
-            this.ultimo = nodo;
+            this.ultimo.siguiente = nodox;
+            nodox.anterior = this.ultimo;
+            this.ultimo = nodox;
         }
     }
-}
 
+    borrarNodo(nodox) {
+        if (nodox != this.primero) {
+            nodox.anterior.siguiente = nodox.siguiente;
+            if (nodox.siguiente != null) {
+                nodox.siguiente.anterior = nodox.anterior;
+            } else {
+                this.ultimo = nodox.anterior;
+            }
+        } else {
+            if (this.primero == this.ultimo) {
+                this.primero = null;
+                this.ultimo = null;
+            } else {
+                this.primero = this.primero.siguiente;
+                this.primero.anterior = null;
+            }
+        }
+    }
+
+    crearArbol() {
+        let aux = this.primero;
+        while (aux != null) {
+            if (aux.valor == '*' || aux.valor == '/') {
+                aux.hijoIzq = aux.anterior;
+                aux.hijoDer = aux.siguiente;
+                this.borrarNodo(aux.anterior);
+                this.borrarNodo(aux.siguiente);
+            }
+            aux = aux.siguiente;
+        }
+        aux = this.primero;
+        while (aux != null) {
+            if (aux.valor == '+' || aux.valor == '-') {
+                aux.hijoIzq = aux.anterior;
+                aux.hijoDer = aux.siguiente;
+                this.borrarNodo(aux.anterior);
+                this.borrarNodo(aux.siguiente);
+            }
+            aux = aux.siguiente;
+        }
+        this.raiz = this.primero;
+
+        return this.raiz;
+    }
+
+    operacionConDosNumeros(operador, num1, num2) {
+        if (operador == '+') return num1 + num2;
+        if (operador == '-') return num1 - num2;
+        if (operador == '*') return num1 * num2;
+    
+        return num1 / num2;
+    }
+
+    preorder(nodox) {
+        if (nodox != null) {
+            this.preorderArr.push(nodox.valor);
+            this.preorder(nodox.hijoIzq);
+            this.preorder(nodox.hijoDer);
+        } 
+
+        if (this.preorderArr[this.preorderArr.length - 1] == undefined) {
+            this.preorderArr.pop();
+        }
+
+        return this.preorderArr;
+    }
+
+    postorder(nodox) {
+        if (nodox != null) {
+            this.postorder(nodox.hijoIzq);
+            this.postorder(nodox.hijoDer);
+            this.postorderArr.push(nodox.valor);
+        }
+
+        return this.postorderArr;
+    }
+
+    calcularPreorder() {
+        let pila = [];
+    
+        for (let i = this.preorderArr.length - 1; i >= 0; i--) {
+          if (!isNaN(this.preorderArr[i])) {
+                pila.push(Number(this.preorderArr[i]));
+                continue
+            }
+    
+        let num1 = pila.pop();
+        let num2 = pila.pop();
+        let resultado = this.operacionConDosNumeros(this.preorderArr[i], num1, num2)
+
+        pila.push(resultado);
+        }
+    
+        return pila.pop();
+    }
+
+    calcularPostorder() {
+        let pila = [];
+    
+        for (let i = 0; i < this.postorderArr.length; i++) {
+          if (!isNaN(this.postorderArr[i])) {
+                pila.push(Number(this.postorderArr[i]));
+                continue
+            }
+    
+        let num1 = pila.pop();
+        let num2 = pila.pop();
+        let resultado = this.operacionConDosNumeros(this.postorderArr[i], num1, num2)
+
+        pila.push(resultado);
+        }
+    
+        return pila.pop();
+    } 
+
+    mostrarPreorderHTML() {
+        let preorder = this.preorder(this.raiz);
+        let resultadoPreorder = this.calcularPreorder();
+
+        return `<h4>Preorder: ${preorder}</h4><h4>Resultado: ${resultadoPreorder}</h4>`;
+    }
+
+    mostrarPostorderHTML() {
+        let postorder = this.postorder(this.raiz);
+        let resultadoPostorder = this.calcularPostorder();
+
+        return `<h4>Postorder: ${postorder}</h4><h4>Resultado: ${resultadoPostorder}</h4>`;
+    }
+}
